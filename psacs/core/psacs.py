64f1082f4a7c551f4f202abddf420cc279757293
chesteric31 <chesteric31@gmail.com>
2014-11-13 12:12:00 +0100

    Fix problem of stop button (set to Enable False at the start).

--- psacs/core/psacs.py
+++ psacs/core/psacs.py
@@ -17,15 +17,14 @@ class psacs(wx.Frame):
         sizer.Add(leftButton, (0, 0))
         
         self.label = wx.StaticText(self, -1, label=u'Hello !')
-        self.label.SetBackgroundColour(wx.BLUE)
-        self.label.SetForegroundColour(wx.WHITE)
         
         MyThread = Clicking(self.label)
         
-        self.Bind(wx.EVT_BUTTON, lambda event: self.OnLeftClick(event, MyThread), leftButton)
         stopButton = wx.Button(self, -1, label="Stop")
+        stopButton.Enable(False)
+        self.Bind(wx.EVT_BUTTON, lambda event: self.OnLeftClick(event, MyThread, stopButton), leftButton)
         sizer.Add(stopButton, (0, 1))
-        self.Bind(wx.EVT_BUTTON, lambda event: self.OnStopClick(event, MyThread), stopButton)
+        self.Bind(wx.EVT_BUTTON, lambda event: self.OnStopClick(event, MyThread, stopButton), stopButton)
 
         sizer.Add(self.label, (1, 0), (1, 2), wx.EXPAND)
 
@@ -34,14 +33,17 @@ class psacs(wx.Frame):
         self.SetSizeHints(-1, self.GetSize().y, -1, self.GetSize().y);
         self.Show(True)
 
-    def OnLeftClick(self, event, MyThread):
+    def OnLeftClick(self, event, MyThread, stopButton):
         self.label.SetLabel("Left Clicking")
         if MyThread.isAlive() == False:
             MyThread.start()
+            stopButton.Enable(True)
+            
         
-    def OnStopClick(self, event, MyThread):
+    def OnStopClick(self, event, MyThread, stopButton):
         self.label.SetLabel("Stop Clicking")
         MyThread.stop()
+        stopButton.Enable(False)
 
 if __name__ == "__main__":
     app = wx.App()
